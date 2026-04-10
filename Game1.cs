efb5acf987f61f52604b12db4cc9896d54f92d1d
Caua-Sudol <caua.rocha@alfatransportes.com.br>
2026-04-09 22:43:29 -0300

    first commit

---
+++ Game1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SnakeGameMonoGame;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Texture2D snake;
+    private Rectangle rec = new Rectangle(0, 0, 20, 20);
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        snake = new Texture2D(GraphicsDevice, 1, 1); 
+        Color[] color = new Color[] { Color.White };
+        snake.SetData(color);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        _spriteBatch.Begin();
+
+        _spriteBatch.Draw(snake, rec, Color.Green);
+        
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new SnakeGameMonoGame.Game1();
+game.Run();
